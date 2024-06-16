1dc9ad2eb619895973773ff651694a7b6d8e364f
DragonFruit8 <Jtbyers19@gmail.com>
2024-06-16 11:39:54 -0400

    Added dotenv, pathlib, and load_dotenv to circumvent the directory issue with not having .env or __pychace__ - Notify me if there are any issues

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,10 @@
 from pydantic_settings import BaseSettings
-
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv(dotenv_path=Path(".env"))
 
 class Settings(BaseSettings):
-    MONGODB_URL: str 
+    MONGODB_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE: int 
     ALGORITHM: str
@@ -10,4 +12,4 @@ class Settings(BaseSettings):
 class Config:
     env_file = ".env"
     
-settings = Settings()    
+settings = Settings()
